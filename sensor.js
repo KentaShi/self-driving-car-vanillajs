821a1c0b8ee4e4c4f0278487bd2e66e5be80f602
Hoang Nam <hoangnam.ml113@gmail.com>
2024-05-19 16:04:05 +0700

    self driving continue

--- sensor.js
+++ sensor.js
@@ -9,14 +9,16 @@ class Sensor {
         this.readings = []
     }
 
-    update(roadBorders) {
+    update(roadBorders, traffic) {
         this.#castRays()
         this.readings = []
         for (let i = 0; i < this.rays.length; i++) {
-            this.readings.push(this.#getReading(this.rays[i], roadBorders))
+            this.readings.push(
+                this.#getReading(this.rays[i], roadBorders, traffic)
+            )
         }
     }
-    #getReading(ray, roadBorders) {
+    #getReading(ray, roadBorders, traffic) {
         let touches = []
         for (let i = 0; i < roadBorders.length; i++) {
             const touch = getIntersection(
@@ -29,6 +31,22 @@ class Sensor {
                 touches.push(touch)
             }
         }
+
+        for (let i = 0; i < traffic.length; i++) {
+            const poly = traffic[i].polygon
+            for (let j = 0; j < poly.length; j++) {
+                const value = getIntersection(
+                    ray[0],
+                    ray[1],
+                    poly[j],
+                    poly[(j + 1) % poly.length]
+                )
+                if (value) {
+                    touches.push(value)
+                }
+            }
+        }
+
         if (touches.length === 0) {
             return null
         } else {
@@ -41,11 +59,12 @@ class Sensor {
     #castRays() {
         this.rays = []
         for (let i = 0; i < this.rayCount; i++) {
-            const rayAngle = lerp(
-                this.raySpread / 2,
-                -this.raySpread / 2,
-                i / (this.rayCount - 1)
-            )
+            const rayAngle =
+                lerp(
+                    this.raySpread / 2,
+                    -this.raySpread / 2,
+                    this.rayCount === 1 ? 0.5 : i / (this.rayCount - 1)
+                ) + this.car.angle
 
             const start = { x: this.car.x, y: this.car.y }
             const end = {
